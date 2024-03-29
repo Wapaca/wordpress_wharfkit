d78069b49a377e5f1d323c8929e884248785fb39
Wapaca <wapaca@gmail.com>
2024-03-29 01:13:30 +0100

    Remove comments

--- widgets/wharfkit_login.php
+++ widgets/wharfkit_login.php
@@ -6,70 +6,23 @@ if (!defined('ABSPATH')) {
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
 
-/**
- * Elementor image box widget.
- *
- * Elementor widget that displays wharfkit login button
- *
- * @since 1.0.0
- */
 class Widget_Wharftkit_Login extends Widget_Base
 {
-    /**
-     * Get widget name.
-     *
-     * Retrieve image box widget name.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget name.
-     */
     public function get_name()
     {
         return 'wharfkit-login';
     }
 
-    /**
-     * Get widget title.
-     *
-     * Retrieve image box widget title.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget title.
-     */
     public function get_title()
     {
         return __('Wordpress Wharfkit Login', 'wordpress-wharfkit');
     }
 
-    /**
-     * Get widget icon.
-     *
-     * Retrieve image box widget icon.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget icon.
-     */
     public function get_icon()
     {
         return 'eicon-image-box';
     }
 
-    /**
-     * Get widget keywords.
-     *
-     * Retrieve the list of keywords the widget belongs to.
-     *
-     * @since 2.1.0
-     * @access public
-     *
-     * @return array Widget keywords.
-     */
     public function get_keywords()
     {
         return ['wax', 'blockchain', 'web3', 'login', 'wharfkit'];
@@ -80,14 +33,6 @@ class Widget_Wharftkit_Login extends Widget_Base
 	}
 
 
-    /**
-     * Render image box widget output on the frontend.
-     *
-     * Written in PHP and used to generate the final HTML.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
     protected function render()
     {
         $appname = get_option('wordpresswharfkit_appname');
@@ -106,14 +51,6 @@ class Widget_Wharftkit_Login extends Widget_Base
     <?php
     }
 
-    /**
-	 * Render image box widget output in the editor.
-	 *
-	 * Written as a Backbone JavaScript template and used to generate the live preview.
-	 *
-	 * @since 1.0.0
-	 * @access protected
-	 */
 	protected function _content_template() {
         $appname = get_option('wordpresswharfkit_appname');
         $chain_id = get_option('wordpresswharfkit_chain_id');
--- wordpresswharfkit.php
+++ wordpresswharfkit.php
@@ -14,49 +14,12 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-/**
- * Main Elementor Test Extension Class
- *
- * The main class that initiates and runs the plugin.
- *
- * @since 1.0.0
- */
 final class WordpressWharfkit_extension
 {
-
-    /**
-     * Plugin Version
-     *
-     * @since 1.0.0
-     *
-     * @var string The plugin version.
-     */
     const VERSION = '1.0.0';
 
-    /**
-     * Instance
-     *
-     * @since 1.0.0
-     *
-     * @access private
-     * @static
-     *
-     * @var WordpressWharfkit_extension The single instance of the class.
-     */
     private static $_instance = null;
 
-    /**
-     * Instance
-     *
-     * Ensures only one instance of the class is loaded or can be loaded.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     * @static
-     *
-     * @return WordpressWharfkit_extension An instance of the class.
-     */
     public static function instance()
     {
 
@@ -67,30 +30,12 @@ final class WordpressWharfkit_extension
 
     }
 
-    /**
-     * Constructor
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
     public function __construct()
     {
         add_action('init', [$this, 'i18n']);
         add_action('plugins_loaded', [$this, 'init']);
     }
 
-    /**
-     * Load Textdomain
-     *
-     * Load plugin localization files.
-     *
-     * Fired by `init` action hook.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
     public function i18n()
     {
 
@@ -98,19 +43,6 @@ final class WordpressWharfkit_extension
 
     }
 
-    /**
-     * Initialize the plugin
-     *
-     * Load the plugin only after Elementor (and other plugins) are loaded.
-     * Checks for basic plugin requirements, if one check fail don't continue,
-     * if all check have passed load the files required to run the plugin.
-     *
-     * Fired by `plugins_loaded` action hook.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
     public function init()
     {
 
@@ -128,15 +60,6 @@ final class WordpressWharfkit_extension
         add_action('elementor/widgets/widgets_registered', [$this, 'init_widgets'], 20);
     }
 
-    /**
-     * Admin notice
-     *
-     * Warning when the site doesn't have Elementor installed or activated.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
     public function admin_notice_missing_main_plugin()
     {
 
@@ -155,12 +78,6 @@ final class WordpressWharfkit_extension
 
     }
 
-    /**
-     * Create Category
-     *
-     * @param [type] $elements_manager
-     * @return void
-     */
     function add_elementor_widget_categories($elements_manager)
     {
 
@@ -235,15 +152,6 @@ final class WordpressWharfkit_extension
       return $attributes;
     }
 
-    /**
-     * Init Widgets
-     *
-     * Include widgets files and register them
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
     public function init_widgets($wm)
     {
 
