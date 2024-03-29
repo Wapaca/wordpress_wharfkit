ba668460a6caa4ab680cfde9a3d30a4509ee3f19
Wapaca <wapaca@gmail.com>
2024-03-29 01:06:37 +0100

    Add settings for transactions

--- widgets/wharfkit_transact.php
+++ widgets/wharfkit_transact.php
@@ -6,70 +6,23 @@ if (!defined('ABSPATH')) {
 use Elementor\Widget_Base;
 use Elementor\Controls_Manager;
 
-/**
- * Elementor image box widget.
- *
- * Elementor widget that displays transact button if user is logged in
- *
- * @since 1.0.0
- */
 class Widget_Wharftkit_Transact extends Widget_Base
 {
-    /**
-     * Get widget name.
-     *
-     * Retrieve image box widget name.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget name.
-     */
     public function get_name()
     {
         return 'wharfkit-transact';
     }
 
-    /**
-     * Get widget title.
-     *
-     * Retrieve image box widget title.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget title.
-     */
     public function get_title()
     {
         return __('Wordpress Wharfkit Transact', 'wordpress-wharfkit');
     }
 
-    /**
-     * Get widget icon.
-     *
-     * Retrieve image box widget icon.
-     *
-     * @since 1.0.0
-     * @access public
-     *
-     * @return string Widget icon.
-     */
     public function get_icon()
     {
         return 'eicon-image-box';
     }
 
-    /**
-     * Get widget keywords.
-     *
-     * Retrieve the list of keywords the widget belongs to.
-     *
-     * @since 2.1.0
-     * @access public
-     *
-     * @return array Widget keywords.
-     */
     public function get_keywords()
     {
         return ['wax', 'blockchain', 'web3', 'transact', 'wharfkit'];
@@ -80,46 +33,82 @@ class Widget_Wharftkit_Transact extends Widget_Base
     }
 
 
-    /**
-     * Render image box widget output on the frontend.
-     *
-     * Written in PHP and used to generate the final HTML.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
+    protected function register_controls() {
+
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => esc_html__( 'Transaction data', 'textdomain' ),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'button_label',
+            [
+                'label' => __('Button label', 'wordpress-wharfkit'),
+                'type' => Controls_Manager::TEXT,
+                'placeholder' => __("Transact", 'wordpress-wharfkit')
+            ]
+        );
+
+        $this->add_control(
+            'transaction_json_data',
+            [
+                'label' => __('Transaction Data', 'wordpress-wharfkit'),
+                'type' => Controls_Manager::TEXTAREA,
+                'placeholder' => __("[{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: %actor_permission%,
+                    data: {
+                        from: %actor_name%,
+                        to: 'we',
+                        quantity: '0.00000001 WAX',
+                        memo: 'wordpress wharfkit first action'
+                    }
+                 },{
+                    account: 'eosio.token',
+                    name: 'transfer',
+                    authorization: %actor_permission%,
+                    data: {
+                        from: %actor_name%,
+                        to: 'we',
+                        quantity: '0.00000001 WAX',
+                        memo: 'wordpress wharfkit second action'
+                    }
+                 }]", 'wordpress-wharfkit'),
+                'description' => __('Enter transaction data object.', 'wordpress-wharfkit'),
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
     protected function render()
     {
         $appname = get_option('wordpresswharfkit_appname');
         $chain_id = get_option('wordpresswharfkit_chain_id');
         $chain_url = get_option('wordpresswharfkit_chain_url');
+
+        $settings = $this->get_settings_for_display();
+
+        $button_label = ( strlen($settings['button_label']) > 0) ? $settings['button_label'] : 'Transact' ;
+
+        $transaction_data = $settings['transaction_json_data'];
+        $transaction_data = str_replace('%actor_name%', 'wharfkit_data.actor_name', $transaction_data);
+        $transaction_data = str_replace('%actor_permission%', '[wharfkit_data.session.permissionLevel]', $transaction_data);
     ?>
         <div class="wordpress-wharfkit-app wharfkit-transact" data-appname="<?= esc_attr( $appname ) ?>" data-chain-id="<?= esc_attr( $chain_id ) ?>" data-chain-url="<?= esc_attr( $chain_url ) ?>">
             <div class="wordpress-wharfkit-vif" data-var="isLoggedIn" data-value="true">
-                <button onclick="wharfkit_transact([{
-                        account: 'eosio.token',
-                        name: 'transfer',
-                        authorization: [wharfkit_data.session.permissionLevel],
-                        data: {
-                            from: wharfkit_data.actor_name,
-                            to: 'waxpaca.fx',
-                            quantity: '0.00000001 WAX',
-                            memo: 'made from wordpress'
-                        }
-                    }])">TEST transact</button>
+                <?php if(strlen($transaction_data)): ?>
+                <button onclick="wharfkit_transact(<?= $transaction_data ?>)"><?= esc_attr( $button_label ) ?></button>
+                <?php endif; ?>
             </div>
         </div>
     <?php
     }
 
-    /**
-     * Render image box widget output in the editor.
-     *
-     * Written as a Backbone JavaScript template and used to generate the live preview.
-     *
-     * @since 1.0.0
-     * @access protected
-     */
     protected function _content_template() {
         $appname = get_option('wordpresswharfkit_appname');
         $chain_id = get_option('wordpresswharfkit_chain_id');
@@ -127,6 +116,10 @@ class Widget_Wharftkit_Transact extends Widget_Base
         ?>
         <div class="wharfkit-transact-backoffice">
             <div>Wordpress Wharfkit Transact</div>
+            <ul>
+                <li>Button label: {{ settings.button_label }}</li>
+                <li>Transaction data: {{ settings.transaction_json_data }}</li>
+            </ul>
             <ul>
                 <li>appName: <?= esc_attr($appname) ?></li>
                 <li>chain_id: <?= esc_attr($chain_id) ?></li>
